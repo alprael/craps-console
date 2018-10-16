981fa96b72ac5c17639f651828fcc0fb50cfdda0
Alex Rael <alprael22@gmail.com>
2018-10-16 16:25:22 -0600

    vcs.xml added

--- src/edu/cnm/deepdive/craps/controller/Main.java
+++ src/edu/cnm/deepdive/craps/controller/Main.java
@@ -1,35 +1,31 @@
 package edu.cnm.deepdive.craps.controller;
 
 import edu.cnm.deepdive.craps.model.Game;
-import edu.cnm.deepdive.craps.model.Game.State;
 import java.security.SecureRandom;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
-  private int wins;
-  private int losses;
   private Random rng;
+  private Game game;
 
   public static void main(String[] args) {
-    new Main().run();
+    Main main = new Main();
+    if (args.length > 0) {
+      main.run(Integer.parseInt(args[0]));
+    } else {
+      main.run();
+    }
   }
 
   private Main() {
-    wins = 0;
-    losses = 0;
     rng = new SecureRandom();
+    game = new Game(rng);
   }
 
   private void play() {
-    Game game = new Game(rng);
-    Game.State state = game.play();
-    if (state == State.WIN) {
-      wins ++;
-    } else {
-      losses++;
-    }
+    game.play();
     for (Game.Roll roll : game.getRolls()) {
      System.out.println(roll);
    }
@@ -39,7 +35,7 @@ public class Main {
     try (Scanner scanner = new Scanner(System.in)) {
       do {
         play();
-        System.out.printf("Wins: %d; Losses: %d.%nPlay again?", wins, losses);
+        System.out.printf("Wins: %d; Losses: %d.%nPlay again?", game.getWins(), game.getLosses());
         String input = scanner.next().trim();
         if (!input.isEmpty() && input.toLowerCase().charAt(0) != 'y') {
           break;
@@ -47,4 +43,16 @@ public class Main {
       } while (true);
     }
   }
+
+  public void run(int trials) {
+    while (trials > 0) {
+      game.play();
+      trials--;
+    }
+    int wins = game.getWins();
+    int losses = game.getLosses();
+    double winPercent = 100.0 * wins / (wins + losses);
+    System.out.printf("Wins: %d; Losses: %d.%nWinnig percentage = %f%%.%n", wins, losses, winPercent);
+  }
+
 }
